4437858c79f4453899ce6f58af3292d1fec153a5
Johnny Wan <wonjohnny@outlook.com>
2020-04-28 01:18:03 -0700

    HW3 automated test suit for measurements generation

--- HW_3/profile_tests.sh
+++ HW_3/profile_tests.sh
@@ -1,20 +1,21 @@
 #! /bin/bash
 
 make
-cat /proc/meminfo | grep "MemTotal" > measurements.log
-printf "Number of processors: " >> measurements.log 
-cat /proc/cpuinfo | grep "processor" | wc -l >> measurements.log
+cat /proc/meminfo | grep "MemTotal" > report/measurements.log
+printf "Number of processors: " >> report/measurements.log 
+cat /proc/cpuinfo | grep "processor" | wc -l >> report/measurements.log
 
-for size in 5 100 114514
+cd compile && for size in 5 100 114514
 do
     for threads in 1 8 30 40
     do
         for i in Synchronized Null Unsynchronized AcmeSafe
         do 
-            echo "" >> measurements.log 
-            echo "$i class with $threads threads, on $size-entry array:" >> measurements.log
-            (time timeout 3600 java UnsafeMemory $i $threads 100000000 $size) &>> measurements.log
+            echo "" >> ../report/measurements.log 
+            echo "$i class with $threads threads, on $size-entry array:" >> ../report/measurements.log
+            (time timeout 3600 java UnsafeMemory $i $threads 100000000 $size) &>> ../report/measurements.log
         done 
     done 
 done
+cd ..
 make clean
